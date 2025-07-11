a71ee812f2ee53cafe328e10b175287e0843cb7d
김예환 Ye-Hwan Kim (Sam) <yehwankim23@gmail.com>
2025-07-11 18:11:23 +0900

    Hide tokens

--- scripts/login.js
+++ scripts/login.js
@@ -1,15 +1,10 @@
+import axios from "axios";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyBsZDKyla0iRBlPvzAH7nnM2g4yZ0Z32qg",
-  authDomain: "calculus-grade-calculato-f5e00.firebaseapp.com",
-  projectId: "calculus-grade-calculato-f5e00",
-  storageBucket: "calculus-grade-calculato-f5e00.appspot.com",
-  messagingSenderId: "300676061",
-  appId: "1:300676061:web:a4d73fd75da03b1b873c34",
-  measurementId: "G-34HY7HMRHN",
-});
+const firebaseApp = initializeApp(
+  (await axios.get("https://tokens.yehwan.kim/tokens")).data.firebase
+);
 
 const analytics = getAnalytics(firebaseApp);
 
--- scripts/oauth.js
+++ scripts/oauth.js
@@ -1,17 +1,11 @@
+import axios from "axios";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, doc, getDoc, setDoc, updateDoc, arrayUnion } from "firebase/firestore";
-import axios from "axios";
 
-const firebaseApp = initializeApp({
-  apiKey: "AIzaSyBsZDKyla0iRBlPvzAH7nnM2g4yZ0Z32qg",
-  authDomain: "calculus-grade-calculato-f5e00.firebaseapp.com",
-  projectId: "calculus-grade-calculato-f5e00",
-  storageBucket: "calculus-grade-calculato-f5e00.appspot.com",
-  messagingSenderId: "300676061",
-  appId: "1:300676061:web:a4d73fd75da03b1b873c34",
-  measurementId: "G-34HY7HMRHN",
-});
+const firebaseApp = initializeApp(
+  (await axios.get("https://tokens.yehwan.kim/tokens")).data.firebase
+);
 
 const analytics = getAnalytics(firebaseApp);
 const firestore = getFirestore(firebaseApp);
